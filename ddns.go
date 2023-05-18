c038fee46d19079a577dcc2a2aea464526ab0cda
soease <scwy@qq.com>
2023-05-18 16:16:44 +0800

    Update ddns.go

--- ddns.go
+++ ddns.go
@@ -8,23 +8,24 @@ import (
 )
 
 func init() {
-	caddy.RegisterModule(Gizmo{})
+	caddy.RegisterModule(&HelloWorld{})
 }
 
 // Gizmo只是一个例子；可以是你自己的类型
-type Gizmo struct {
+type HelloWorld struct {
 }
 
 // 通过CaddyModule方法返回Caddy模块的信息
-func (Gizmo) CaddyModule() caddy.ModuleInfo {
+func (HelloWorld) CaddyModule() caddy.ModuleInfo {
 	return caddy.ModuleInfo{
-		ID:  "ddns",
-		New: func() caddy.Module { return new(Gizmo) },
+		ID:  "http.handlers.hello_world",
+		New: func() caddy.Module { return new(HelloWorld) },
 	}
 }
 
 // 实现Handler接口
-func (g *Gizmo) ServeHTTP(w http.ResponseWriter, r *http.Request) error {
+func (g *HelloWorld) ServeHTTP(w http.ResponseWriter, r *http.Request) error {
 	fmt.Fprint(w, "Hello, World!")
 	return nil
 }
+
